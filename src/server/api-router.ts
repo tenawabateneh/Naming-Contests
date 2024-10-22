5a8d767cc6a997e9f3b4278e2033dc8abff46a85
tenaw <tenawabateneh06@gmail.com>
2024-10-22 11:28:32 +0300

    successful working with data which is directly from real mongoDB data

--- src/server/api-router.ts
+++ src/server/api-router.ts
@@ -1,14 +1,37 @@
 import express from "express"
 import cors from "cors"
 
-import testData from '../local.contests.json'
+import { connectClient } from "./db"
 
 const router = express.Router()
 router.use(cors())
 
 // Create mock API endpoint
-router.get("/contests", (req, res) => {
-    res.send({ contests: testData })
+router.get("/contests", async (req, res) => {
+    const client = await connectClient();
+
+    const contests = await client
+        .collection("contests")
+        .find()
+        .project({
+            id: 1,
+            categoryName: 1,
+            contestName: 1,
+            _id: 0,
+        })
+        .toArray();
+    res.send({ contests })
+})
+
+// Getting a single contest by ID
+router.get("/contest/:contestId", async (req, res) => {
+    const client = await connectClient();
+
+    const contest = await client
+        .collection("contests")
+        .findOne({ id: req.params.contestId })
+
+    res.send({ contest })
 })
 
 export default router
--- src/server/config.ts
+++ src/server/config.ts
@@ -1,6 +1,6 @@
 const env = process.env;
 
-export const PORT = env.PORT ?? "3030";
+export const PORT = env.PORT ?? "8080";
 export const HOST = env.HOST ?? "127.0.0.1";
 export const SERVER_URL = `http://${HOST}:${PORT}`;
 
